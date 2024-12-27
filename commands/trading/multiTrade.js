b28b730017dd790fa174c30c3b8f15c8d5cd6023
luna <linkzersmash@gmail.com>
2024-12-26 18:18:44 -0700

    Changed multi trade

--- commands/trading/multiTrade.js
+++ commands/trading/multiTrade.js
@@ -1,30 +1,30 @@
-const { EmbedBuilder, AttachmentBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, ComponentType } = require("discord.js");
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, ComponentType } = require("discord.js");
 const { Users, UserCards, CardDatabase, ItemShop } = require("../../dbObjects");
-const allCards = require("../../packs/allCards.json");
-const { rarityStars } = require("../../pullingObjects.js");
+const { formatName, raritySymbol } = require("../../pullingObjects.js");
 
 
-function makeEmbed(user, otherUser, userTrade, otherUserTrade) {
+function makeEmbed(user, otherUser, userTrade, otherUserTrade, checkUser) {
+    
     const tradeEmebed = new EmbedBuilder()
-        .setTitle("Multi Trade")
-        .setDescription(`${user}\'s items:\n${userTrade.join("\n")}\n\n${otherUser}\'s items:\n${otherUserTrade.join("\n")}`)
+        .setTitle(`Multiple Trade [${user.username} <--> ${otherUser.username}]`)
+        .setDescription(`**${user}\'s items:**${checkUser == 0 ? " ✅" : ""}\n\`\`\`diff\n${userTrade.length ? userTrade.join("\n") : "- No items seleceted..."}\`\`\`\n**${otherUser}\'s items:**${checkUser == 1  ? " ✅" : ""}\n\`\`\`diff\n${otherUserTrade.length ? otherUserTrade.join("\n") : "- No items seleceted..."}\`\`\`\nTo add a \`Card\`, type its card code. **EX:** aa0000\nTo remove a \`Card\`, type its card code again. **EX:** aa0000\n\nTo add an \`Item\`, type its name followed it its amount.**EX:** Draw 5 x2\nTo remove an \`Item\`, type its name and its amount as 0. **EX:** Draw 5 x0\n*The \"x\" in front of the amount is required*\n\nTo add multiple cards or items at a time, sperate each thing using commas. \n**EX:** aa0000, aa0001, Draw 5 x2`)
     return tradeEmebed;
 }
 
 function makeEmbedCancel(user, otherUser, userTrade, otherUserTrade) {
     const tradeEmebed = new EmbedBuilder()
-        .setTitle("Card Trade")
+        .setTitle("Multiple Trade [Canceled]")
         .setColor("#bd0f0f")
-        .setDescription(`${user}\'s items:\n${userTrade.join("\n")}\n\n${otherUser}\'s items:\n${otherUserTrade.join("\n")}\n\n**Trade was canceled.**`)
+        .setDescription(`**${user}\'s items:**\n\`\`\`diff\n${userTrade.length ? userTrade.join("\n") : "- No items seleceted..."}\`\`\`\n**${otherUser}\'s items:**\n\`\`\`diff\n${otherUserTrade.length ? otherUserTrade.join("\n") : "- No items seleceted..."}\`\`\`\n\n**Trade was canceled.**`)
 
     return tradeEmebed;
 }
 
 function makeEmbedConfirm(user, otherUser, userTrade, otherUserTrade) {
     const tradeEmebed = new EmbedBuilder()
-        .setTitle("Card Trade")
+        .setTitle("Multiple Trade [Finished]")
         .setColor("#26bd0f")
-        .setDescription(`${user}\'s items:\n${userTrade.join("\n")}\n\n${otherUser}\'s items:\n${otherUserTrade.join("\n")}\n\n**Trade was accepted.**`)
+        .setDescription(`**${user}\'s items:** ✅\n\`\`\`diff\n${userTrade.length ? userTrade.join("\n") : "- No items seleceted..."}\`\`\`\n**${otherUser}\'s items:** ✅\n\`\`\`diff\n${otherUserTrade.length ? otherUserTrade.join("\n") : "- No items seleceted..."}\`\`\`\n\n**Trade was accepted.**`)
 
     return tradeEmebed;
 }
@@ -89,122 +89,60 @@ function findItem(collection, itemName) {
     return null;
 }
 
-async function generateUserTradeList(splitMessage, start, message) {
-    if (splitMessage[start][0] != "\"") {
-        await message.channel.send(`${message.author} both list of items/cards must be inclosed in \" \".`);
-        return null;
-    }
+function getItemData(splitMessage) {
+    let itemNameList = [];
+    let itemAmount = 1;
 
-    tradeList = "";
-
-    for (let i = start; i < splitMessage.length; i++) {
-        sub = splitMessage[i];
-        if (sub[sub.length - 1] == "\"") {
-            tradeList += ` ${((sub.split("\"").filter(function (el) {
-                return el != '';
-            })[0]))}`;
-            return tradeList.trim();
-        }
-        else if (sub[0] == "\"") {
-           sub = sub.split("\"")[1];
-        }
-
-        tradeList += ` ${sub}`;
-    }
-
-    await message.channel.send(`${message.author} both list of items/cards must be inclosed in \" \".`);
-    return null;
-} 
-
-async function checkTradeOffer(from, user, userAt, message, splitMessage) {
-    let tradeList = (await generateUserTradeList(splitMessage, from, message)).split(",");
-    if (!tradeList) return;
-
-    let userTrade = {};
-    for (let i = 0; i < tradeList.length; i++) {
-        tradeItem = tradeList[i].trim();
-
-        if (tradeItem.length == 6) {
-            const cardInfo = findCard(await user.getCards(), tradeItem);
-            if (!cardInfo) {
-                await message.channel.send(`That card is not in ${userAt}\'s collection.`); 
-                return;
-            }
-            userTrade[tradeItem] = [cardInfo, "1"];
+    for (m of splitMessage) {
+        if (m.toLowerCase()[0]== "x" && !isNaN(m.toLowerCase().split("x")[1])) {
+            itemAmount = m.toLowerCase().split("x")[1];
             continue;
         }
-        
-        let itemList = tradeList[i].split(" ");
 
-        let itemName = "";
-        let quanity = "";
-        for (let itemSub of itemList) {
-            if ((itemSub.toLowerCase())[0] == "x") {
-                quanity = (itemSub.toLowerCase()).split("x")[1];
-                break;
-            }
-
-            itemName += ` ${itemSub}`;
-        }
-        itemName = (itemName.trim()).toUpperCase();
-
-        if (!(await ItemShop.findOne({ where: { name: itemName } }))) {
-            await message.channel.send(`${message.author} the item ${itemName} is misspelt.`);
-            return;
-        }
-
-        const itemInfo = findItem(await user.getItems(), itemName);
-
-        if (!itemInfo || itemInfo.amount < sub.substring(1)) {
-            await message.channel.send(`${userAt} dosen't have enough of that item.`); 
-            return;
-        }
-        userTrade[itemName] = [itemInfo, quanity];
+        itemNameList.push(m);
     }
 
-    return userTrade;
+    const itemName = itemNameList.join(" ").toUpperCase();
+    
+    return [itemName, itemAmount];
 }
 
-function getSecondTradeStart(splitMessage, message) {
-    let count = 0
-    for (let i = 0; i < splitMessage.length; i++) {
-        if (splitMessage[i][0] == "\"") count++;
+function isInTradeCard(userTradeData, cardInfo) {
+    if (userTradeData.length == 0) { return -1; }    
 
-        if (count == 2) return i;
+    for (index in userTradeData) {
+        const _cardI = userTradeData[index];
+        if (_cardI.item_id == cardInfo.item_id) {
+            return index;
+        }
     }
-    message.channel.send(`${message.author} both list of items/cards must be inclosed in \" \".`)
+
     return -1;
 }
 
-function getFormatUserTrade(userTrade) {
-    let formatedUserTrade = []
-    Object.entries(userTrade).forEach(([name, data]) => {
-        if (name.length == 6) {
-            const pokemonData = allCards[data[0].item.name]
-            formatedUserTrade.push(`\`x${data[1]}\` **${pokemonData['CardID']}-${pokemonData['Name']}** - ${rarityStars(pokemonData['Rarity'])} - \`${data[0].item_id}\``);
+function isInTradeItem(userTradeData, itemInfo) {
+    if (userTradeData.length == 0) { return null; }
+    
+    for (_itemI of userTradeData) {
+        if (_itemI[0].item_id == itemInfo.item_id) {
+            return _itemI;
         }
-        else {
-            formatedUserTrade.push(`\`x${data[1]}\` ${data[0].item.emoji} \`${data[0].item.name}\``);
-        }
-    });
-
-    return formatedUserTrade;
+    }
+    return null;
 }
 
-async function transferTradeItems(otherUser, userTrade) {
-    await Object.entries(userTrade).forEach(async ([name, data]) => {
-        if (name.length == 6) {
-            data[0].user_id = otherUser.user_id;
-            data[0].save();
-        }
-        else {
-            const itemData = await ItemShop.findOne({ where: { name: data[0].item.name } });
-            data[0].amount -= data[1];
-            data[0].save();
+function transferTradeItems(otherUser, userTradeData) {
+    for (card of userTradeData["Cards"]) {
+        card.user_id = otherUser.user_id;
+        card.save();
+    }
 
-            otherUser.addItem(itemData, data[1]);
-        }
-    });
+    for (itemData of userTradeData["Items"]) {
+        itemData[0].amount -= itemData[1];
+        itemData[0].save();
+
+        otherUser.addItem(itemData[0].item, itemData[1]);
+    }
 }
 
 module.exports = {
@@ -213,75 +151,155 @@ module.exports = {
     shortName: ['mt'],
         
     async execute(message) {
-        let confirm = false;
         const splitMessage = message.content.split(" ");
 
-        if (splitMessage.length < 4) {
-            await message.channel.send(`${message.author}, you must specify the person your trading to, your list of cards/items, and their list of cards/items.`);
+        if (splitMessage.length < 2) {
+            await message.channel.send({ content: `${message.author}, you must specify the person you are trading with.` });
             return;
-        }        
+        }
 
         const user = await Users.findOne({ where: { user_id: message.author.id } });
-        if ((await user.getCards()).length == 1) {
-            await message.channel.send(`${message.author} you can't multi trade with only one card.`);
-            return;
+        const userTrade = []
+        const userTradeData = {
+            "Cards": [], // card
+            "Items": []  // [item, amount]
         }
 
         const otherUser = await Users.findOne({ where : { user_id: message.mentions.users.first().id } });
-        if ((await otherUser.getCards()).length == 1) {
-            await message.channel.send(`${message.mentions.users.first()} can't be multi trade with when they have only one card.`);
-            return;
+        const otherUserTrade = []
+        const otherUserTradeData = {
+            "Cards": [], // card
+            "Items": []  // [item, amount]
         }
 
+        if (!otherUser) {
+            await message.channel.send({ content: `${message.author}, that user can not be found. They must register first before they can be traded to or they do not exist.` });
+            return;
+        }
+        
         if (user.user_id == otherUser.user_id) {
             await message.channel.send(`${message.author}, you can not trade to yourself.`);
             return;
         }
+        let otherConfirmUser;
+        let checkUser = -1;
 
-        if (!otherUser) {
-            await message.channel.send(`${message.author}, that user can not be found. They must register first before they can be traded to or they do not exist.`);
-            return;
-        }
+        const response = await message.channel.send({ embeds: [makeEmbed(message.author, message.mentions.users.first(), userTrade, otherUserTrade, checkUser)], components: [makeButton()] });
 
-        let userTrade = await checkTradeOffer(2, user, message.author, message, splitMessage);
-        if (!userTrade) return;
-        const secondStart = getSecondTradeStart(splitMessage, message);
-        if (secondStart == -1) return;
-        let otherUserTrade = await checkTradeOffer(secondStart, otherUser, message.mentions.users.first(), message, splitMessage);
-        if (!otherUserTrade) return;
+        const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 120_000 });
+        const messageCollector = message.channel.createMessageCollector({ time: 120_000 });
 
-        const formatUserTrade = getFormatUserTrade(userTrade);
-        const formatOtherUserTrade = getFormatUserTrade(otherUserTrade);
+        // MESSAGE HANDLER
+        messageCollector.on('collect', async m => {
+            let _tradeUser;
+            let _tradeUserTrade;
+            let _tradeUserTradeData;
+            if (m.author.id == user.user_id) { 
+                _tradeUser = user;
+                _tradeUserTrade = userTrade; 
+                _tradeUserTradeData = userTradeData 
+            }
+            else if (m.author.id == otherUser.user_id) { 
+                _tradeUser = otherUser;
+                _tradeUserTrade = otherUserTrade; 
+                _tradeUserTradeData = otherUserTradeData 
+            }
+            else { return; }
 
+            const splitM = m.content.split(",");
+            otherConfirmUser = null;
+            let checkUser = -1;
+            m.delete();
 
-        const response = await message.channel.send({ embeds: [makeEmbed(message.author, splitMessage[1], formatUserTrade, formatOtherUserTrade)], components: [makeButton()] });
+            for (_m of splitM) {
+                const _splitM = _m.trim().split(" ");
 
-        const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 15_000 });
+                if (_splitM.length == 1 && _splitM[0].length == 6) {
+                    const cardInfo = findCard(await _tradeUser.getCards(), _splitM[0].toLowerCase());
+    
+                    if (!cardInfo) {
+                        continue;
+                    }
+
+                    const existingCard = isInTradeCard(_tradeUserTradeData["Cards"], cardInfo);
+                    if (existingCard > -1) {
+                        _tradeUserTradeData["Cards"].splice(existingCard, 1);
+
+                        const cardTextIndex = _tradeUserTrade.indexOf(`x1 ${formatName(cardInfo.item)} - ${raritySymbol(cardInfo.item.rarity)} - ${cardInfo.item_id}`);
+                        _tradeUserTrade.splice(cardTextIndex, 1);
+                        continue;
+                    }
+    
+                    _tradeUserTradeData["Cards"].push(cardInfo);
+                    _tradeUserTrade.push(`x1 ${formatName(cardInfo.item)} - ${raritySymbol(cardInfo.item.rarity)} - ${cardInfo.item_id}`);
+    
+                }
+                else {
+                    const itemData = getItemData(_splitM);
+                    const itemInfo = findItem(await _tradeUser.getItems(), itemData[0]);
+
+                    if (!itemInfo || itemInfo.amount < itemData[1]) {
+                        continue;
+                    }
+
+                    const existingItem = isInTradeItem(_tradeUserTradeData["Items"], itemInfo);
+                    if (existingItem) {
+                        if (itemData[1] == "0") {
+                            const itemTextIndex = _tradeUserTrade.indexOf(`x${existingItem[1]} ${existingItem[0].item.name}`);
+                            _tradeUserTrade.splice(itemTextIndex, 1);
+
+                            const itemIndex = _tradeUserTradeData["Items"].indexOf(existingItem);
+                            _tradeUserTradeData["Items"].splice(itemIndex, 1);
+                            continue;
+                        }
+
+                        const itemTextIndex = _tradeUserTrade.indexOf(`x${existingItem[1]} ${existingItem[0].item.name}`);
+                        existingItem[1] += Number.parseInt(itemData[1]);
+                        _tradeUserTrade[itemTextIndex] = `x${existingItem[1]} ${existingItem[0].item.name}`;
+                        continue;
+                    }
+                    
+                    _tradeUserTradeData["Items"].push([itemInfo, Number.parseInt(itemData[1])]);
+                    _tradeUserTrade.push(`x${itemData[1]} ${itemInfo.item.name}`);
+                }
+            }
+            
+            await response.edit({ embeds: [makeEmbed(message.author, message.mentions.users.first(), userTrade, otherUserTrade, checkUser)], components: [makeButton()] });
+        });
+
+        messageCollector.on("end", async i => {
+            await response.edit({ embeds: [makeEmbedCancel(message.author, message.mentions.users.first(), userTrade, otherUserTrade)], components: [] });
+        });
+
+        // BUTTON HANDLER
 
         collector.on("collect", async i => {
-            if (i.user == message.mentions.users.first() || i.user == message.author) {
-                if(i.customId == "cancel") {
-                    await response.edit({ embeds: [makeEmbedCancel(message.author, splitMessage[1], formatUserTrade, formatOtherUserTrade)], components: [] });
-                    i.deferUpdate();
-                }
-            }
-            if (i.user == message.mentions.users.first()) {
-                if (i.customId == "confirm") {
-                    await response.edit({ embeds: [makeEmbed(message.author, splitMessage[1], formatUserTrade, formatOtherUserTrade)], components: [makeButtonConfirm()] });
-                    i.deferUpdate();
-                }
-            }
-            else if (i.user == message.author) {
-                if (i.customId == "finalConfirm") {
+            if (i.user.id == user.user_id) { null }
+            else if (i.user.id == otherUser.user_id) { null }
+            else { i.deferUpdate(); return; }
 
-                    transferTradeItems(otherUser, userTrade);
-                    transferTradeItems(user, otherUserTrade);
+            if (i.customId == "confirm") {
+                if (i.user.id == user.user_id) { otherConfirmUser = otherUser; checkUser = 0; }
+                else if (i.user.id == otherUser.user_id) { otherConfirmUser = user; checkUser = 1; }
 
-                    await response.edit({ embeds: [makeEmbedConfirm(message.author, splitMessage[1], formatUserTrade, formatOtherUserTrade)], components: [] });
-                    i.deferUpdate();
-                }
+                await response.edit({ embeds: [makeEmbed(message.author, message.mentions.users.first(), userTrade, otherUserTrade, checkUser)], components: [makeButtonConfirm()] });
+                i.deferUpdate();
             }
-        
+
+            else if (i.customId == "finalConfirm" && i.user.id == otherConfirmUser.user_id) {
+
+                transferTradeItems(otherUser, userTradeData);
+                transferTradeItems(user, otherUserTradeData);
+
+                messageCollector.stop()
+                await response.edit({ embeds: [makeEmbedConfirm(message.author, message.mentions.users.first(), userTrade, otherUserTrade)], components: [] });
+                i.deferUpdate();
+            }
+
+            else if (i.customId == 'cancel') {
+                messageCollector.stop()
+                i.deferUpdate();
+            }
         });
     },
 };
