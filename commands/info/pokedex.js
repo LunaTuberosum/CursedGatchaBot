52f6ebb962b783ca84df8f32dae5c3f83fb3f42b
luna <linkzersmash@gmail.com>
2025-01-17 22:45:46 -0700

    g!pokeDex now can show series dex

--- commands/info/pokedex.js
+++ commands/info/pokedex.js
@@ -1,6 +1,7 @@
 const { EmbedBuilder, AttachmentBuilder, ComponentType, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require("discord.js");
-const { CardDatabase, Wishlists } = require("../../dbObjects");
+const { CardDatabase, Wishlists, Users } = require("../../dbObjects");
 const { formatName, raritySymbol, makePokeImage } = require("../../pullingObjects");
+const allCards = require("../../packs/allCards.json");
 
 function makeEmbed(pokemonData, wishlistInfo) {
 
@@ -32,6 +33,49 @@ function makeButton() {
     return row;
 }
 
+function makeSeriesEmbed(seriesDict, series) {
+
+    const embed = new EmbedBuilder()
+        .setColor("#616161")
+        .setTitle(`Pokedex - ${series}`)
+        .setDescription(`${Object.values(seriesDict).join("\n")}`)
+
+    return embed;
+}
+
+async function _makeSeriesInfo(message, series, response) {
+    let seriesDict = {}
+
+    for (card of Object.keys(allCards[series])) {
+        if (card[0] == '*' || card[0] == '[' || card[0] == '{') continue;
+        const cardData = allCards[series][card];
+
+        
+        const pokemonData = await CardDatabase.findOne({ where: { name: cardData["Name"] } });
+
+        seriesDict[card] = `${formatName(pokemonData)} - \`${raritySymbol(pokemonData.rarity)}\``;
+        
+    }
+
+    const user = await Users.findOne({ where: { user_id: message.author.id } });
+    if (!user) {
+        await response.edit({ content: " ", embeds: [makeSeriesEmbed(seriesDict, series)] });
+        return;
+    }
+
+    const userCards = await user.getCards();
+
+    for (uCard of userCards) {
+        if (uCard.item.series == series) {
+            if (seriesDict[uCard.item.name][seriesDict[uCard.item.name].length - 1] == "✅") { continue; }
+            seriesDict[uCard.item.name] = seriesDict[uCard.item.name] + " - ✅";
+        }
+        
+    }
+
+    await response.edit({ content: " ", embeds: [makeSeriesEmbed(seriesDict, series)] });
+}
+
 module.exports = {
     name: "pokedex",
     shortName: ["pd"],
@@ -43,10 +87,18 @@ module.exports = {
 
         if (splitMessage.length != 2) { await response.edit(`${message.author}, please enter the name of the pokemon you'd like to lookup`); return; }
 
+        if (splitMessage[1].length == 4) {
+            if (splitMessage[1].toUpperCase() == "EVE1")
+                _makeSeriesInfo(message, "EVE1", response);
+
+            else { await response.edit(`${message.author}, that pokemon either dosen't exist or its name is misspelt.`); return; }
+            return;
+        }
+
         const pokeName = `${splitMessage[1][0].toUpperCase()}${splitMessage[1].substr(1).toLowerCase()}`;
         
         const pokeList = await CardDatabase.findAll({ where: { name: pokeName } });
-        if (pokeList.length == 0) { await response.edit(`${message.author}, that pokemon either dosen't exist or its name is misspelt.`); return; }
+        if (pokeList.length == 0 || !pokeList) { await response.edit(`${message.author}, that pokemon either dosen't exist or its name is misspelt.`); return; }
 
         let pokeIndex = 0;
 
