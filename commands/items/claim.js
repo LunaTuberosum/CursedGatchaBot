4d986a7f67cc29997e71dbe3f17b9c51cccbb35a
luna <linkzersmash@gmail.com>
2025-01-18 07:17:37 -0700

    g!claim done

--- commands/items/claim.js
+++ commands/items/claim.js
@@ -1,18 +1,31 @@
+const { Users, ItemShop } = require("../../dbObjects");
+
 const claimedUsers = []
+
 
 module.exports = {
     name: "claim",
 
     async execute(message) {
+        // // For disabling the command
+        // return;
+
         const response = await message.channel.send("loading claim...");
 
+        const user = await Users.findOne({ where: { user_id: message.author.id } });
+        if (!user) { await response.edit(`${message.author}, you can not claim the maintenance bonus if you are not registered. Please register with \`g!register\`.`); return; }
+
         if (claimedUsers.includes(message.author.id)) {
-            await message.channel.send(`- ${claimedUsers.join("")}`);
+            await response.delete();
             return;
         }
 
+        await response.edit(`${message.author}, thank you for your patience during this latest maintenance ! Here are a free \`20 POKEDOLLARS\`!`);
+
+        const itemData = await ItemShop.findOne({ where: { name: "POKEDOLLAR" } });
+        user.addItem(itemData, 20);
+
         claimedUsers.push(message.author.id);
-        await message.channel.send(`-- ${claimedUsers.join(" ")}`);
 
     }
 }
