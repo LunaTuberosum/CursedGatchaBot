6fb663c332987b9ead54a5cf0cbe67d7b1764d29
luna <linkzersmash@gmail.com>
2025-01-18 09:05:02 -0700

    fixed pokeshop (#2)

--- commands/shop/pokeShop.js
+++ commands/shop/pokeShop.js
@@ -61,7 +61,7 @@ async function getArray(shopItems, start, end) {
 }
 
 function getLength(shopData) {
-    let length = 0;
+    let length = 1;
     for (const item of shopData) {
         if (item.cost != 0) length += 1
     }
@@ -100,8 +100,8 @@ module.exports = {
 
                     }
                     else if (i.customId == "right") {
-                        start = Math.min(start + 5, length - 4);
-                        end = Math.min(end + 5, length);
+                        start = Math.min(start + 5, length - 5);
+                        end = Math.min(end + 5, length - 1);
                         checkButtons(buttons, start, end, length);
 
                         shopArray = await getArray(shopData, start, end);
