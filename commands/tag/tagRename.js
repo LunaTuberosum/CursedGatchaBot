b5962b72a067017f08ab4e21a8f95106c15ce7bc
luna <linkzersmash@gmail.com>
2025-01-24 20:39:43 -0700

    g!tagRename now changes cards with its tag's name (#12)

--- commands/tag/tagRename.js
+++ commands/tag/tagRename.js
@@ -1,5 +1,5 @@
 const { splitContent } = require('../../commandObjects');
-const { Tags } = require('../../dbObjects');
+const { Tags, Users } = require('../../dbObjects');
 
 module.exports = {
     cooldown: 5,
@@ -8,6 +8,9 @@ module.exports = {
         
     async execute(message) {
         const splitMessage = splitContent(message);
+
+        const user = await Users.findOne({ where: { user_id: message.author.id } });
+        if (!user) { await response.edit(`${message.author}, you are not registered. Please register using \`g!register\`.`); return; }
 
         if (splitMessage.length != 3) {
             message.channel.send(`${message.author}, you must specify both the orinal name and the new name.`);
@@ -28,6 +31,15 @@ module.exports = {
         const tag = await Tags.findOne({ where: { user_id: message.author.id, name: splitMessage[1] } });
 
         if (!tag) { await message.channel.send(`${message.author}, please send a vaild tag name`); return; }
+
+        const userCards = await user.getCards();
+
+        for (const card of userCards) {
+            if (card.tag == tag.name) {
+                card.tag = splitMessage[2];
+                card.save();
+            }
+        }
 
         tag.name = splitMessage[2];
         tag.save();
