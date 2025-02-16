ed0ed87ceb0c222f2847f5b27072c07088ddfb30
luna <linkzersmash@gmail.com>
2025-02-16 07:17:21 -0700

    Made dbBackup.js

---
+++ dbBackup.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+const { Tags, Users, UserCards, UserStats, UserTitles, TitleDatabase, UserDailys, CardDatabase, UserItems, ItemShop, ServerInfo, Wishlists } = require('./dbObjects');
+    
+async function backup() {
+    // CREATE DICT BACK UP
+    let databaseDict = {
+        "users" : await Users.findAll({ where: { }, raw: true }),
+        "userStats" : await UserStats.findAll({ where: { }, raw: true }),
+        "userTitles" : await UserTitles.findAll({ where: { }, raw: true }),
+        "titleDatabase" : await TitleDatabase.findAll({ where: { }, raw: true }),
+        "userCards" : await UserCards.findAll({ where: { }, raw: true }),
+        "userDailys" : await UserDailys.findAll({ where: { }, raw: true }),
+        "cardDatabase" : await CardDatabase.findAll({ where: { }, raw: true }),
+        "userItems" : await UserItems.findAll({ where: { }, raw: true }),
+        "itemShop" : await ItemShop.findAll({ where: { }, raw: true }),
+        "serverInfo" : await ServerInfo.findAll({ where: { }, raw: true }),
+        "wishlists" : await Wishlists.findAll({ where: { }, raw: true }),
+        "tags" : await Tags.findAll({ where: { }, raw: true })
+    }
+    
+    // WRITE DATA BASE BACK UP
+    const dictString = JSON.stringify(databaseDict, null, "\t");
+    fs.writeFile("databaseBackup.json", dictString, function(err, result) {
+        if(err) console.log('error', err);
+    });
+}
+
+backup();
--- dbUpdate.js
+++ dbUpdate.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
 var fs = require('fs');
-const { log } = require('console');
 
 allCards = require("./packs/allCards.json");
 
@@ -30,28 +29,6 @@ async function changeData(databaseDict) {
 }
 
 async function print() {
-
-    // CREATE DICT BACK UP
-    // let databaseDict = {
-    //     "users" : await Users.findAll({ where: { }, raw: true }),
-    //     "userStats" : await UserStats.findAll({ where: { }, raw: true }),
-    //     "userTitles" : await UserTitles.findAll({ where: { }, raw: true }),
-    //     "titleDatabase" : await TitleDatabase.findAll({ where: { }, raw: true }),
-    //     "userCards" : await UserCards.findAll({ where: { }, raw: true }),
-    //     "userDailys" : await UserDailys.findAll({ where: { }, raw: true }),
-    //     "cardDatabase" : await CardDatabase.findAll({ where: { }, raw: true }),
-    //     "userItems" : await UserItems.findAll({ where: { }, raw: true }),
-    //     "itemShop" : await ItemShop.findAll({ where: { }, raw: true }),
-    //     "serverInfo" : await ServerInfo.findAll({ where: { }, raw: true }),
-    //     "wishlists" : await Wishlists.findAll({ where: { }, raw: true }),
-    //     "tags" : await Tags.findAll({ where: { }, raw: true })
-    // }
-    
-    // // WRITE DATA BASE BACK UP
-    // const dictString = JSON.stringify(databaseDict, null, "\t");
-    // fs.writeFile("databaseBackup.json", dictString, function(err, result) {
-    //     if(err) console.log('error', err);
-    // });
 
     let databaseDict = JSON.parse(fs.readFileSync("databaseBackup.json"));
     
